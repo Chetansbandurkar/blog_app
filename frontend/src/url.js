effa2ac604ca574b36bc4450e461245ee0d94be8
Chetansbandurkar <csbandurkar2003@gmail.com>
2024-07-16 18:38:37 +0530

    final

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,3 @@
 // export const URL="http://localhost:5000"
-export const URL="https://blogapp-backend-79rw.onrender.com"
-export const IF="https://blogapp-backend-79rw.onrender.com/images/"
+export const URL="https://blog-app-yrnu.onrender.com"
+export const IF="https://blog-app-yrnu.onrender.com/images/"
